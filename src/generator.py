851d12ed562a31a8035909444fcfa86d6b047cae
Eric Bulloch <ericbulloch@users.noreply.github.com>
2025-05-04 16:12:43 -0500

    Update generator.py
    Added generate_dates function

--- src/generator.py
+++ src/generator.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 import random
 
 def generate_date():
@@ -5,3 +6,11 @@ def generate_date():
     minute = str(random.randint(0, 59))
     second = str(random.randint(0, 59))
     return f'{hour.zfill(2)}:{minute.zfill(2)}:{second.zfill(2)}'
+
+def generate_dates(days_ago):
+    now = datetime.now()
+    dates = []
+    for i in range(days_ago + 1):
+        date = now - timedelta(days=i)
+        dates.append(date.strftime('%Y-%m-%d'))
+    return dates[::-1]
