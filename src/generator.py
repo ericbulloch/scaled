72b3dbfb103af94c0f77406ecf56dbbcd0deb6cd
Eric Bulloch <ericbulloch@users.noreply.github.com>
2025-05-04 16:14:56 -0500

    Update generator.py
    Added generate_orders function

--- src/generator.py
+++ src/generator.py
@@ -1,5 +1,9 @@
 from datetime import datetime, timedelta
+import json
+import os
 import random
+
+from source import get_items, get_tenants, read_postal_csv
 
 def generate_date():
     hour = str(random.randint(0, 23))
@@ -14,3 +18,56 @@ def generate_dates(days_ago):
         date = now - timedelta(days=i)
         dates.append(date.strftime('%Y-%m-%d'))
     return dates[::-1]
+
+def generate_orders(date, amount):
+    parent_dir = 'orders'
+    file_name = f'{os.path.join(parent_dir, date)}.json'
+    if os.path.exists(file_name):
+        with open(file_name, 'r') as fp:
+            return json.load(fp)
+    tenants = get_tenants()
+    items = get_items()
+    postal = read_postal_csv('postal.csv')
+    orders = []
+    if not os.path.exists(parent_dir):
+        os.makedirs(parent_dir)
+    tenant_weights = [0.49] + [0.075] * 4 + [0.04] * 5 + [0.0005] * 20
+    for i in range(amount):
+        tenant = random.choices(tenants, weights=tenant_weights)[0]
+        print(tenant)
+        postal_data = random.choice(postal)
+        timestamp = f'{date}T{generate_date()}Z'
+        number_of_items = random.randint(1, 15)
+        order_items = []
+        for j in range(number_of_items):
+            item = random.choice(items)
+            quantity = random.randint(1, 30)
+            order_items.append({
+                "id": item['id'],
+                "name": item['name'],
+                "quantity": quantity,
+                "price": item['price'],
+                "cost": item['cost'],
+            })
+        order = {
+            "postal": postal_data['postal'],
+            "state": postal_data['state'],
+            "timestamp": timestamp,
+            "tenant_uuid": tenant["id"],
+            "tenant_name": tenant['name'],
+            "items": order_items,
+        }
+        orders.append(order)
+    with open(file_name, 'w') as fp:
+        json.dump(orders, fp, indent=4)
+    return orders
+
+
+if __name__ == '__main__':
+    dates = generate_dates(days_ago=60)
+    for d in dates:
+        current = datetime.strptime(d, '%Y-%m-%d')
+        number_of_orders = 10 if current.weekday() > 4 else 30
+        orders = generate_orders(d, number_of_orders)
+        print(json.dumps(orders, indent=4))
+        print(f"{number_of_orders} orders generated for {d}")
