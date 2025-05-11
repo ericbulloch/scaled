b92c6f40ef90e9c5bb30511a3a44e5999ddb16e4
Eric Bulloch <ericbulloch@users.noreply.github.com>
2025-05-11 16:36:59 -0500

    Update server.py
    Creating and tearing down database connection

--- src/server.py
+++ src/server.py
@@ -1,1 +1,24 @@
+import sqlite3
 
+from flask import Flask, g
+
+DATABASE = 'database.db'
+app = Flask(__name__)
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.route('/')
+def index():
+    return 'working'
+
+
+@app.teardown_appcontext
+def close_db(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
