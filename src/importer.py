ed708d906929485c5e651c88fd297dc193749428
Eric Bulloch <ericbulloch@users.noreply.github.com>
2025-05-12 12:55:19 -0500

    Update importer.py
    Added function to map an order

--- src/importer.py
+++ src/importer.py
@@ -1,1 +1,12 @@
+import json
 
+
+def map_order(order):
+    return tuple([
+        order['tenant_uuid'],
+        order['tenant_name'],
+        order['postal'],
+        order['state'],
+        order['timestamp'],
+        json.dumps(order['items']),
+    ])
